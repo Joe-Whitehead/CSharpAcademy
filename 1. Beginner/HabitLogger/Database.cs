7b70c6edbc4c5a7988a2c43ddc1299a670a03415
Joe <joe980@gmail.com>
2024-03-25 11:03:07 +0000

    Added Database Class | Basic User Menu

---
+++ 1. Beginner/HabitLogger/Database.cs
@@ -0,0 +1,56 @@
+﻿using System.Data.SQLite;
+
+namespace HabitLogger;
+
+internal class Database
+{
+    private string _connectionString = "DataSource=Habit-Logger.db";
+    public SQLiteConnection DbConnection = new SQLiteConnection();
+
+    public Database()
+    {
+        DbConnection = new SQLiteConnection(_connectionString);
+        DbConnection.Open();      
+    }
+
+    public void CreateTablesIfNotExist()
+    {
+        using var cmd = new SQLiteCommand(DbConnection);
+
+        //Habits Table
+        cmd.CommandText = "DROP TABLE IF EXISTS Habits";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE TABLE Habits(id INTEGER PRIMARY KEY, Name TEXT, Unit TEXT)";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = @"INSERT INTO Habits(Name, Unit) VALUES ('Water', 'Glasses'), ('Fruit', 'Portions')";
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Habits Table Created");
+
+        //Water Table
+        cmd.CommandText = "DROP TABLE IF EXISTS Water";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE TABLE Water(id INTEGER PRIMARY KEY, Quantity INTEGER, DateTime TEXT)";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = $"INSERT INTO Water(Quantity, DateTime) VALUES(2,'{DateTime.UtcNow}')";
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Water Table Created");
+
+        //Fruit Table
+        cmd.CommandText = "DROP TABLE IF EXISTS Fruit";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE TABLE Fruit(id INTEGER PRIMARY KEY, Quantity, DateTime TEXT)";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = $"INSERT INTO Fruit(Quantity, DateTime) VALUES(1, '{DateTime.UtcNow}')";
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+
+}
--- 1. Beginner/HabitLogger/Program.cs
+++ 1. Beginner/HabitLogger/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HabitLogger;
+bool endAPp = false;
+Database db = new Database();
+
+while (!endAPp)
+{    
+    int menuSelection;
+
+    Console.WriteLine("""
+        Habit Tracker
+        -------------
+        MAIN MENU
+
+        0 - Close Application
+        1 - View All Records
+        2 - Insert Record
+        3 - Delete Record
+        4 - Update Record
+        -------------
+        """);
+    menuSelection = ValidateMenu();
+    Console.Clear();
+
+    switch (menuSelection)
+    {
+        case 0:
+            endAPp = true;
+            Console.WriteLine("Goodbye");
+            break;
+
+        case 1:
+            //TODO: View Records
+            Console.WriteLine("View Records");
+            break;
+        case 2:
+            //TODO: Inseert Record
+            Console.WriteLine("Insert Record");
+            break;
+        case 3:
+            //TODO: Delete Record
+            Console.WriteLine("Delete Record");
+            break;
+        case 4:
+            //TODO: Update Record
+            Console.WriteLine("Update Record");
+            break;
+    }
+}
+
+int ValidateMenu()
+{
+    string? input = Console.ReadLine();
+    int validNumber;
+    while (!int.TryParse(input, out validNumber))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Value must be a number: ");
+        Console.ResetColor();
+        input = Console.ReadLine();
+    }
+    return validNumber;
+
+}
